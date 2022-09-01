9d62dee48ab230d6c48b91afa387c80d6a27c044
Decoder-01 <afeezishola41@gmail.com>
2022-08-31 21:47:56 -0600

    My OOP

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-class Item:
+class Item: #My first class
 
-    def __init__(self, name: str, price: int, quantity=0):
-        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >= 0, f"quantity {quantity} is not greater or equal to zero!"
+    def __init__(self, name: str, price: int, quantity=0): # I created a method to take three parameters for a which is name price and quantity
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!" #this function of this assert keyword is to give an error message if the price parameter is less than 0
+        assert quantity >= 0, f"quantity {quantity} is not greater or equal to zero!" # Likewise this also for the quantity cause it shouldn't be less than zero
 
         self.name = name
         self.price = price
